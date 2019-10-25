05113b4cb7527917fbb6a35ec51d79bdc486ccfd
Christian Ivicevic <christian.ivicevic@comsysto.com>
2019-10-25 13:16:29 +0200

    Replace @TypeHint annotation with colon notation

--- src/transformers/asciidocPlugins/typeHintPlugin.ts
+++ src/transformers/asciidocPlugins/typeHintPlugin.ts
@@ -1,6 +1,6 @@
 export const parseTypeHints = (content: string) =>
     content.replace(
         // Replace /* @TypeHint(type) */ directives with the corresponding type hints in the code.
-        /<span class="token punctuation-definition-comment-ts(?:x)?"(?:.*?)>\/\*(?:.*?)@TypeHint\((.*)\)(?:.*?)\*\/<\/span>/g,
+        /<span class="token punctuation-definition-comment-ts(?:x)?"(?:.*?)>\/\*<\/span><span(?:.*?)>(?:\s*?):(?:\s*?)(.*?)<\/span><span(?:.*?)>\*\/<\/span>/g,
         '<span class="type-hint">: $1</span>'
     );
