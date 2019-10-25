02794ee7475276a26645a2d013e2a6c2384b52e3
Christian Ivicevic <christian.ivicevic@comsysto.com>
2019-10-25 15:32:47 +0200

    Remove unnecessary space in TypeHints

--- src/transformers/asciidocPlugins/typeHintPlugin.ts
+++ src/transformers/asciidocPlugins/typeHintPlugin.ts
@@ -2,5 +2,5 @@ export const parseTypeHints = (content: string) =>
     content.replace(
         // Replace /* @TypeHint(type) */ directives with the corresponding type hints in the code.
         /<span class="token punctuation-definition-comment-ts(?:x)?"(?:.*?)>\/\*<\/span><span(?:.*?)>(?:\s*?):(?:\s*?)(.*?)<\/span><span(?:.*?)>\*\/<\/span>/g,
-        '<span class="type-hint">: $1</span>'
+        '<span class="type-hint">:$1</span>'
     );
